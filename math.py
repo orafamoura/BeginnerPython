80603d7a9aefb31cf75e089f2f50af64ca6024dc
orafamoura <raphaelmvb2@gmail.com>
2024-01-24 14:11:54 -0300

    math

---
+++ math.py
@@ -0,0 +1,20 @@
+friends = 5
+
+friends += 5
+print(friends)
+
+friends -= 5
+print(friends)
+
+friends *= 2
+print(friends)
+
+friends /= 2
+friends = int(friends)
+print(friends)
+
+friends **= 2
+print(friends)
+
+remainder = friends % 3
+print(remainder)
